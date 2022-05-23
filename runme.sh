cc5314d8265d86bdab169e59c9d23b004f6729c7
Muzzammil Shaikh <muzzammil194@gmail.com>
2022-05-23 15:50:36 +0500

    env/runme setting

--- runme.sh
+++ runme.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run  -t --rm --env-file .env \
-  --network dhis2-netspace dhis2-loader
+docker run  -t --rm --env-file /home/akdndhrc/muzzammil/projects/Dhis2/.env \
+  --network dhis-apis_dhis-netspace dhis2-loader:latest
